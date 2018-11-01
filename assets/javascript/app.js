1887bad47ef55536b0bb87d6a18f88604fb228f5
Chen <achen@c02vmakphtd5.hbo.homebox.com>
2018-11-01 17:09:13 -0400

    image test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,31 @@
 window.onload = function() {
   dogapp.getdogs()
+//   $(".gif").on("click", function() {
+
+//     if(state === "still") {
+//       var animateurl = $(this).attr("data-animate")
+//       $(this).attr("src", animateurl)
+//       $(this).attr("data-state", "animate")
+//     }
+//     if(state === "animate") {
+//       var stillurl = $(this).attr("data-still")
+//       $(this).attr("src", stillurl)
+//       $(this).attr("data-state", "still")
+//     }
+    
+// });
+
+    $(".dog-card").on("click", function() {
+      dogapp.getgifs($(this).attr("data-name"),9)
+      
+    });
+
 };
 
-$(".gif").on("click", function() {
+// Global Variables
 
-    if(state === "still") {
-      var animateurl = $(this).attr("data-animate")
-      $(this).attr("src", animateurl)
-      $(this).attr("data-state", "animate")
-    }
-    if(state === "animate") {
-      var stillurl = $(this).attr("data-still")
-      $(this).attr("src", stillurl)
-      $(this).attr("data-state", "still")
-    }
-    
-});
-
+var gifgroup = 0
+var giphydogs;
 var dogs = [
   "maltese", 
   "shiba", 
@@ -33,7 +42,7 @@ var dogapp = {
         // Then dynamicaly generating buttons
         var dcard = $("<div>");
         // Adding a class to our button
-        dcard.addClass("dog-card card");
+        dcard.addClass("dog-card btn btn-secondary btn-block text-left");
         // Adding a data-attribute
         dcard.attr("data-name", currentdog);
         // Providing the initial button text
@@ -44,5 +53,33 @@ var dogapp = {
         // Adding the button
         $("#dog-area").append(dcard);
     }
+  },
+  getgifs : function(d, maxCategories) {
+    var gifCount = 0;
+    function getNextGif() {
+        var currentgif = d
+        if(gifCount < 10) {
+        var dtrim = d.replace(/\s/g, '_');
+        $.ajax({
+            url: "https://api.giphy.com/v1/gifs/search?q=" + d + "_dog" + "&offset=" + gifOffset + "&limit=1&api_key=dc6zaTOxFJmzC",
+            method: "GET",
+            success: function(response) {
+                if (gifCount <= maxCategories) {
+                    gifOffset++;
+                    gifCount++;
+                    console.log(gifOffset)
+                    console.log(response)
+                    dimage = $("<img>")
+                    dimage.attr("src",response.data[0].images.fixed_height_still.url)
+                    $("#gif-area").append(dimage)
+                    getNextGif()
+                    }
+            
+    
+            }
+        })
+      }
   }
+      getNextGif();
+  },
 }
